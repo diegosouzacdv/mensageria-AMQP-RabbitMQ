aa8aea1a0b79bf6920d2fffd7b5b7ce0f1b7d792
Diego de souza pereira <37152874+diegosouzacdv@users.noreply.github.com>
2022-09-29 07:08:02 -0300

    Fanout Exchange

---
+++ coupon-service/src/main/java/com/algaworks/example/coupon/config/RabbitMQConfig.java
@@ -0,0 +1,62 @@
+package com.algaworks.example.coupon.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String ORDER_EXCHANGE_NAME = "order.v1.order-paid";
+    public static final String COUPON_QUEUE_NAME = "coupon.v1.order-paid.generate-coupon";
+
+    Queue couponQueueName = new Queue(COUPON_QUEUE_NAME);
+
+    @Bean
+    public Queue queue() {
+        return couponQueueName;
+    }
+
+    @Bean
+    public Binding binding() {
+        FanoutExchange fanoutExchange = new FanoutExchange(ORDER_EXCHANGE_NAME);
+        return BindingBuilder.bind(couponQueueName).to(fanoutExchange);
+    }
+
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener(
+            RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter(ObjectMapper objectMapper) {
+        objectMapper.findAndRegisterModules();
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+
+}
